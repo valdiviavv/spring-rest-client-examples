5cea4f2623aa264873622645aaec6db2714fb1da
Veronica <valdiviavv@hotmail.com>
2020-10-18 14:17:50 -0400

    The files from the POJO converter were added as well as some tests

---
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -0,0 +1,26 @@
+package guru.springframework.springrestclientexamples.services;
+
+import guru.springframework.api.domain.User;
+import guru.springframework.api.domain.UserData;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class ApiServiceImpl implements  ApiService {
+
+    private RestTemplate restTemplate;
+
+    public ApiServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public List<User>getUsers(Integer limit) {
+
+        UserData userData = restTemplate.getForObject("http://private-anon-752562693e-apifaketory.apiary-proxy.com/api/user?limit=" + limit, UserData.class);
+        return userData.getData();
+    }
+
+}
